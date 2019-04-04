2fe35a9498216c1c5cb1309420cc0e8e142776e2
Irene Hsieh <hsiehiisuan@gmail.com>
2019-04-04 14:20:49 -0700

    changed welcome.html to index.html and hosted our project!

---
+++ setup_firebase_team5.js
@@ -0,0 +1,16 @@
+(function() {
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDDVzoSJOT7n9uISZeY9UpTqB6MmYgQPU0",
+    authDomain: "project-1930.firebaseapp.com",
+    databaseURL: "https://project-1930.firebaseio.com",
+    projectId: "project-1930",
+    storageBucket: "project-1930.appspot.com",
+    messagingSenderId: "763891066897"
+  };
+  firebase.initializeApp(config);
+</script>
+
+}) ();
